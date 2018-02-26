ab08ca83e62fb79beb101647817b5817520165a6
Kirk Willens <kirkwillens@Kirks-MacBook-Pro.local>
2018-02-25 23:47:26 -0600

    receives SYN and sends SYNACK but not getting ACK back

--- tcp_module.cc
+++ tcp_module.cc
@@ -23,6 +23,7 @@ using std::endl;
 using std::cerr;
 using std::string;
 
+void build_packet(Packet &p, ConnectionToStateMapping<TCPState> &Conn_to_State, unsigned int size, unsigned char flag);
 
 int main(int argc, char *argv[])
 {
@@ -47,19 +48,23 @@ int main(int argc, char *argv[])
 
   MinetEvent event;
 
+  // List of connection to state mappings 
+  ConnectionList<TCPState> clist;
   while (MinetGetNextEvent(event)==0) {
     // if we received an unexpected type of event, print error
     if (event.eventtype!=MinetEvent::Dataflow 
 	|| event.direction!=MinetEvent::IN) {
       MinetSendToMonitor(MinetMonitoringEvent("Unknown event ignored."));
+      cerr << "invalid event from Minet" << endl;
     // if we received a valid event from Minet, do processing
     } else {
-      cerr << "invalid event from Minet" << endl;
       //  Data from the IP layer below  //
       if (event.handle==mux) {
+        cerr << "\n---------------\n";
+        cerr << "RECEIVED PACKET FROM BELOW:\n";
+
         Packet p;
         MinetReceive(mux,p);
-        cerr << "received packet from below\n";
         unsigned tcphlen=TCPHeader::EstimateTCPHeaderLength(p);
         cerr << "estimated header len="<<tcphlen<<"\n";
         p.ExtractHeaderFromPayload<TCPHeader>(tcphlen);
@@ -68,77 +73,167 @@ int main(int argc, char *argv[])
 
         Connection c;
 
-        ipl.GetDestIP(c.dest);
-        ipl.GetSourceIP(c.src);
+        ipl.GetDestIP(c.src);
+        ipl.GetSourceIP(c.dest);
         ipl.GetProtocol(c.protocol);
-        tcph.GetDestPort(c.destport);
-        tcph.GetSourcePort(c.srcport); 
+        tcph.GetDestPort(c.srcport);
+        tcph.GetSourcePort(c.destport); 
 
-        ConnectionList<TCPState> clist;
-        ConnectionList<TCPState>::iterator cs = clist.FindMatching(c);
+        cerr << "IP: " << c.src << " to " << c.dest << endl;
+        cerr << "Port: " << c.srcport << " to " << c.destport << endl;
+
         unsigned short len;
         unsigned char tcp_len;
         unsigned char ip_len;
+        unsigned char flag;
+        unsigned int seqnum;
+
 
         ipl.GetTotalLength(len);
         ipl.GetHeaderLength(ip_len);
         tcph.GetHeaderLen(tcp_len);
+        tcph.GetFlags(flag);
+        tcph.GetSeqNum(seqnum);
 
         len -= (4*tcp_len + 4*ip_len);
-
-        // bool checksumok;
-
+        cerr << "Data Size: " << len << endl;
         Buffer &data = p.GetPayload().ExtractFront(len);
-        // SockRequestResponse write(WRITE,
-        //             (*cs).connection,
-        //             data,
-        //             len,
-        //             EOK);
 
-        SockRequestResponse write (WRITE,(*cs).connection, data, len, EOK);
-
-        MinetSend(sock,write);
-
-        // cs = clist.FindMatching(c);
-        // if (cs!=clist.end()) {
-        //     tcph.GetLength(len);
-        //     len-=TCP_HEADER_LENGTH;
-        //     Buffer &data = p.GetPayload().ExtractFront(len);
-        //     SockRequestResponse write(WRITE,
-        //             (*cs).connection,
-        //             data,
-        //             len,
-        //             EOK);
-        //     if (!checksumok) {
-        //       MinetSendToMonitor(MinetMonitoringEvent("forwarding packet to sock even though checksum failed"));
-        //     }
-        //     MinetSend(sock,write);
-        //   } else {
-        //     MinetSendToMonitor(MinetMonitoringEvent("Unknown port, sending ICMP error message"));
-        //     IPAddress source; iph.GetSourceIP(source);
-        //     ICMPPacket error(source,DESTINATION_UNREACHABLE,PORT_UNREACHABLE,p);
-        //     MinetSendToMonitor(MinetMonitoringEvent("ICMP error message has been sent to host"));
-        //     MinetSend(mux, error);
-        //   }
+        ConnectionList<TCPState>::iterator cs = clist.FindMatching(c);
+        if(cs!=clist.end()) {
+          cerr << "In connections list" << endl;  
+          unsigned int cur_state = cs->state.GetState();
+          cerr << "State: " << cur_state << endl;
 
 
-        cerr << "IP: "<<c.src<< " to "<<c.dest;
-        cerr << "Port "<<c.srcport<< " to "<<c.destport;
+          switch(cur_state) {
+            case LISTEN: {
+              cerr << "Entered LISTEN" << endl;
+              if(IS_SYN(flag)) {
+                cerr << "Received a SYN" << endl;
+                cs->state.SetState(SYN_RCVD);
+                cs->connection = c;
+                cs->state.last_acked = cs->state.last_sent;
+                cs->state.SetLastRecvd(seqnum + 1);
+                cs->bTmrActive = true;
+                cs->timeout = Time() + 8;
+                cs->state.last_sent = cs->state.last_sent + 1;
 
-        // cerr << "TCP Packet: IP Header is "<<ipl<<" and ";
-        // cerr << "TCP Header is "<<tcph << " and ";
+                Packet p_send;
+                unsigned char send_flag = 0;
+                SET_SYN(send_flag);
+                SET_ACK(send_flag);
+                build_packet(p_send,*cs,send_flag,0);
+                cerr << p_send << endl;
+                MinetSend(mux,p_send);
+              }
+              break;
+            }
+            case SYN_RCVD: {
+              cerr << "Entered SYN_RCVD" << endl;
+              if(IS_SYN(flag)) {
+                cerr << "Still SYN" << endl;
+              }
+              if(IS_ACK(flag)) {
+                cerr << "Received an ACK" << endl;
+                cs->state.SetState(ESTABLISHED);
+                SockRequestResponse write(WRITE,
+                        (*cs).connection,
+                        data,
+                        len,
+                        EOK);
+                MinetSend(sock,write);
+              }
+              break;
+            }
 
-        cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID");
+          }
+          
+
+          // bool checksumok;
+
+
+          cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID") << endl;
+        }
+        else {
+          cerr << "Not in connections list" << endl;
+        }
+
+        cerr << "---------------\n";
         
       }
           //  Data from the Sockets layer above  //
       if (event.handle==sock) {
+        cerr << "\n---------------\n";
         SockRequestResponse s;
+        SockRequestResponse response;
         MinetReceive(sock,s);
-        cerr << "received packet from above\n";
+        cerr << "RECEIVED PACKET FROM ABOVE:\n";
         cerr << "Received Socket Request:" << s << endl;
+        switch(s.type) {
+          case ACCEPT: {
+            // passive open from remote. The connection should be fully bound on
+            // the local side and unbound on the remote side. The data, bytes count, and error
+            // fields are ignored. The TCP module will do the passive open and immediately
+            // return a STATUS with only the error code set. Whenever a connection arrives,
+            // the TCP module will accept it and send a zero byte WRITE with the fully bound
+            // connection. 
+            cerr << "ACCEPT request" << endl;
+
+            // Add connection state pair to connections list
+            TCPState new_state(1,LISTEN,3);
+            ConnectionToStateMapping<TCPState> Conn_to_State(s.connection,Time(),new_state,false);
+            cerr << "New connection: " << Conn_to_State << endl;
+            clist.push_back(Conn_to_State);
+
+            // Send STATUS response
+            response.connection = s.connection;
+            response.type = STATUS;
+            response.bytes = 0;
+            response.error = EOK;
+            MinetSend(sock,response);
+            cerr << "Sent Accept response" << endl; 
+
+            break;
+          }
+
+        }
+        cerr << "---------------\n";
       }
     }
   }
   return 0;
 }
+
+
+void build_packet(Packet &p, ConnectionToStateMapping<TCPState> &Conn_to_State, unsigned int size, unsigned char flag) {
+  IPHeader iph;
+  TCPHeader tcph;
+  Connection c = Conn_to_State.connection;
+  int total_size = size + TCP_HEADER_BASE_LENGTH + IP_HEADER_BASE_LENGTH;
+  unsigned char nflag = 0;
+  SET_ACK(nflag);
+  SET_SYN(nflag);
+
+  iph.SetSourceIP(c.src);
+  iph.SetDestIP(c.dest);
+  iph.SetProtocol(IP_PROTO_TCP);
+  iph.SetTotalLength(total_size);
+  p.PushFrontHeader(iph);
+  tcph.SetSourcePort(c.srcport, p);
+  tcph.SetDestPort(c.destport, p);
+  tcph.SetFlags(nflag, p);
+  tcph.SetHeaderLen(5,p);
+  tcph.SetUrgentPtr(0,p);
+  tcph.SetAckNum(Conn_to_State.state.GetLastRecvd(), p);
+  tcph.SetWinSize(Conn_to_State.state.GetN(), p);
+  tcph.SetSeqNum(Conn_to_State.state.GetLastSent()+1,p);
+  tcph.RecomputeChecksum(p);
+  p.PushBackHeader(tcph);
+  cerr << iph << endl;
+  cerr << tcph << endl;
+}
+
+
+
+
